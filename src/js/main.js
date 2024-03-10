dd7c9424141987101d0aa3c5dccf07c0432981d0
polopo927 <polopo927@mail.ru>
2024-03-10 20:40:44 +0300

    второй проект, табы

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,12 @@
 import './slider';
 //подключаем наше модальное окно из папки modules
 import modals from './modules/modals';
+//импортируем табы как функцию
+import tabsChooseBalcons from './modules/tabs';
 
 window.addEventListener('DOMContentLoaded', () => {
 	//обязательно вызываем функцию для её работы
 	modals();
+	tabsChooseBalcons('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+	tabsChooseBalcons('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
 });
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -63,7 +63,7 @@ const modals = () => {
 
 	bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
 	bindModal('.phone_link', '.popup', '.popup .popup_close');
-	showModalByTime('.popup', 5000);
+	//showModalByTime('.popup', 5000);
 };
 
 //для того чтобы этот код можно было экспортировать прописываем данную строчку
